9ab0f4c3c8659198c5c41d8fe1fc69dd9a5a9f7a
Shivi Agarwal <sagarwal2896@gmail.com>
2021-07-05 23:44:34 +0530

    Show user's current location on map.

--- FindNearByPlaces/Controllers/MainViewController.swift
+++ FindNearByPlaces/Controllers/MainViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 import MapKit
 import Firebase
+import CoreLocation
 
 class MainViewController: UIViewController {
     
     let searchVC = UISearchController(searchResultsController: SearchPlacesViewController())
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
     let mapView = MKMapView()
 
     override func viewDidLoad() {
@@ -21,6 +24,7 @@ class MainViewController: UIViewController {
         searchVC.searchBar.backgroundColor = .secondarySystemBackground
         searchVC.searchResultsUpdater = self
         validateAuth()
+        checkLocationServices()
     }
     
     private func validateAuth()
@@ -38,7 +42,65 @@ class MainViewController: UIViewController {
         super.viewDidLayoutSubviews()
         mapView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.size.width, height: view.frame.size.height - view.safeAreaInsets.top)
     }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            // Show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // Show an alert letting them know what's up
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    }
+
+
+extension MainViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
 }
+
 
 extension MainViewController: UISearchResultsUpdating
 {
